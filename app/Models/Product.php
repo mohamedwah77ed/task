aea8477db939fbab03903cb172a5a9cc0b0e58f1
Mohamed Waheed <mo777376@gmail.com>
2026-07-02 20:00:48 +0300

    Add model and migration

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Product extends Model
+{
+    use HasFactory, HasUuids, SoftDeletes;
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'sku',
+        'name',
+        'description',
+        'price',
+        'stock_quantity',
+        'low_stock_threshold',
+        'status',
+    ];
+
+}
